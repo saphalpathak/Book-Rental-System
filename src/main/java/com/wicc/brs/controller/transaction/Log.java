7b2d839d7465ac2037286515dc6a3a00d72a0308
saphalpathak <saphalpathak3@gmail.com>
2022-03-11 13:02:30 +0545

    added transaction log

---
+++ src/main/java/com/wicc/brs/controller/transaction/Log.java
@@ -0,0 +1,24 @@
+package com.wicc.brs.controller.transaction;
+
+import com.wicc.brs.service.transaction.TransactionImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/transaction")
+public class Log {
+    private final TransactionImpl transaction;
+
+    public Log(TransactionImpl transaction) {
+        this.transaction = transaction;
+    }
+
+    @GetMapping("/log")
+    public String log(Model model){
+        model.addAttribute("log",transaction.findAll());
+        return "/transaction/log";
+    }
+
+}
